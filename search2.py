b50b0b6097481bbde51a200cda7a39186e418e6e
Arendelle <2381642961@qq.com>
2025-05-24 15:16:25 +0800

    Fix script

--- search2.py
+++ search2.py
@@ -4,7 +4,7 @@ from physicsLab import web
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-user = web.User(
+user = web.token_login(
     token="dqQXBDflrOYV4a82HMbNjFtz9k3C5hWL",
     auth_code="aUKltj1Nq7JOz0EWHDnXoYP6fk3G4rRd",
 )
@@ -12,19 +12,22 @@ user = web.User(
 with open(os.path.join(SCRIPT_PATH, "all_id")) as f:
     lst: list = eval(f.read())
 
-with open(os.path.join(SCRIPT_PATH, "index")) as f:
-    index = int(f.read())
+if os.path.exists(os.path.join(SCRIPT_PATH, "index")):
+    with open(os.path.join(SCRIPT_PATH, "index")) as f:
+        index = int(f.read())
+else:
+    index = 0
 
 def main():
     try:
         for i, user_id in enumerate(lst[index:]):
-            for a_user in web.RelationsIter(user, user_id, "Following", max_retry=15):
+            for a_user in web.RelationsIter(user, user_id=user_id, display_type="Following", max_retry=15):
                 if a_user["User"]["ID"] not in lst:
                     lst.append(a_user["User"]["ID"])
-            for a_user in web.RelationsIter(user, user_id, "Follower", max_retry=15):
+            for a_user in web.RelationsIter(user, user_id=user_id,  display_type="Follower", max_retry=15):
                 if a_user["User"]["ID"] not in lst:
                     lst.append(a_user["User"]["ID"])
-            print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] index: {index + i}, amount: {len(lst)}  ", end='\r')
+            print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] index: {index + i}, amount: {len(lst)}", flush=True)
     finally:
         with open(os.path.join(SCRIPT_PATH, "all_id"), "w") as f:
             f.write(str(lst))
@@ -37,8 +40,6 @@ if __name__ == "__main__":
             main()
         except KeyboardInterrupt:
             break
-        except:
-            pass
         else:
             break
     print("")
