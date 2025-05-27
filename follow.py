825666836eed83ea6bb46a4c7976059bc981d97b
Arendelle <2381642961@qq.com>
2025-05-27 08:39:48 +0800

    Update follow.py

--- follow.py
+++ follow.py
@@ -1,8 +1,9 @@
 import os
-import asyncio
-from physicsLab import web, ResponseFail
+import time
+from physicsLab import web
+from physicsLab.web._threadpool import ThreadPool, _Task
 
-user = web.User(
+user = web.token_login(
     token="dqQXBDflrOYV4a82HMbNjFtz9k3C5hWL",
     auth_code="aUKltj1Nq7JOz0EWHDnXoYP6fk3G4rRd",
 )
@@ -10,23 +11,21 @@ user = web.User(
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(SCRIPT_PATH, "all_id")) as f:
-    all_id: list = eval(f.read())
+    all_id: list = eval(f.read())[44_000:]
 
-with open(os.path.join(SCRIPT_PATH, "last_all_id")) as f:
-    last_all_id: set = eval(f.read())
+with ThreadPool(max_workers=4) as pool:
+    tasks: list[_Task] = []
+    for id in all_id:
+        tasks.append(pool.submit(user.follow, id))
+    pool.submit_end()
 
-# for a_id in all_id:
-#     if a_id not in last_all_id:
-#         try:
-#             user.follow(a_id)
-#         except ResponseFail as e:
-#             print(a_id, e)
-
-async def main():
-    tasks = []
-    for a_id in all_id:
-        if a_id not in last_all_id:
-            tasks.append(asyncio.create_task(user.async_follow(a_id)))
-    await asyncio.gather(*tasks)
-
-asyncio.run(main())
+    i = 0
+    while i < len(tasks):
+        a_task = tasks[i]
+        try:
+            a_task.result()
+        except Exception as e:
+            print('\n', e, flush=True)
+        else:
+            print(f"{i}/{len(tasks)}          ", end='\r', flush=True)
+            i += 1
